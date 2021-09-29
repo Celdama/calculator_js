ed9275224bb3d92e93d0157fb55034286f864ddd
Celdama <celdama.dev@gmail.com>
2021-09-29 11:25:45 +0200

    reset all code

--- app.js
+++ app.js
@@ -1,62 +1,32 @@
 const displayResult = document.getElementById('display-result');
-const numValue = Array.from(document.querySelectorAll('.numValue'));
+const numsBtn = Array.from(document.querySelectorAll('.numBtn'));
 const addBtn = document.getElementById('addBtn');
 const resultBtn = document.getElementById('resultBtn');
 
-let displayValue = '';
-let firstOperande = 0;
-let secondOperande = 0;
-let calcOperator = '';
+const displayValue = null;
+const firstOperande = null;
+const secondOperande = null;
+const result = null;
+const calcOperator = null;
 
-addBtn.addEventListener('click', () => {
-  firstOperande = Number(displayValue);
-  displayValue = '';
-  calcOperator = '+';
-});
-
-const populateDisplay = (num) => {
-  displayResult.textContent = num;
+const add = function (a, b) {
+  return a + b;
 };
 
-numValue.forEach((num) => {
-  num.addEventListener('click', () => {
-    displayValue += num.textContent;
-    populateDisplay(displayValue);
-  });
-});
-
-const add = (a, b) => {
-  displayValue = +a + +b;
-  populateDisplay(displayValue);
-  // console.log(displayValue);
+const substract = function (a, b) {
+  return a - b;
 };
 
-const substrat = (a, b) => a - b;
+const multiply = function (a, b) {
+  return a * b;
+};
 
-const multiply = (a, b) => a * b;
+const divide = function (a, b) {
+  return a / b;
+};
 
-const divide = (a, b) => a / b;
-
-const operate = (operator, a, b) => {
-  switch (operator) {
-    case '+':
-      add(a, b);
-      break;
-    case '-':
-      substrat(a, b);
-      break;
-    case '*':
-      multiply(a, b);
-      break;
-    case '/':
-      divide(a, b);
-      break;
-    default:
-      break;
+const operate = function (operator, firstOperande, secondOperande) {
+  if (operator === '+') {
+    return add(firstOperande, secondOperande);
   }
 };
-
-resultBtn.addEventListener('click', () => {
-  secondOperande = displayValue;
-  operate(calcOperator, firstOperande, secondOperande);
-});
